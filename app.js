1da82c04378f354d9624efb4e190d4e2079252c0
pc611652003 <88979797+pc611652003@users.noreply.github.com>
2021-11-14 06:41:17 -0700

    added validate to prompts

--- app.js
+++ app.js
@@ -16,12 +16,28 @@ const promptUser = () => {
                 {
                     type: 'input',
                     name: 'name',
-                    message: 'What is your name?'
+                    message: 'What is your name?',
+                    validate: nameInput => {
+                      if (nameInput) {
+                        return true;
+                      } else {
+                        console.log('Please enter your name!');
+                        return false;
+                      }
+                    }
                 },
                 {
                   type: 'input',
                   name: 'github',
-                  message: 'Enter your GitHub Username'
+                  message: 'Enter your GitHub Username',
+                  validate: gitHubNameInput => {
+                    if (gitHubNameInput) {
+                      return true;
+                    } else {
+                      console.log('Please enter your GitHub Username!');
+                      return false;
+                    }
+                  }
                 },
                 {
                   type: 'input',
@@ -45,12 +61,28 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project?',
+        validate: projectNameInput => {
+          if (projectNameInput) {
+            return true;
+          } else {
+            console.log('Please enter the name of your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log('Please enter a description of the project!');
+            return false;
+          }
+        }
       },
       {
         type: 'checkbox',
@@ -61,7 +93,15 @@ const promptProject = portfolioData => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: gitHubLinkInput => {
+          if (gitHubLinkInput) {
+            return true;
+          } else {
+            console.log('Please enter the GitHub link to your project!');
+            return false;
+          }
+        }
       },
       {
         type: 'confirm',
